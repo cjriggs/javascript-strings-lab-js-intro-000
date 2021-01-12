f1fad33f5ffae844bbd2375bc8913c4722e0c7e5
Charles Riggs <riggs.charles.cr@gmail.com>
2021-01-12 19:41:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = conversation + "space.";
 
 var conversation = `${topic}`;
 
+var topic = conversation + "space.";
